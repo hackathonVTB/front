249c7869fb6bb2f92c23ab2b2662c4a60756b670
Maksim Doronkin <maksim3602@gmail.com>
2023-10-13 20:24:47 +0300

    add helpers

--- src/shared/ui/map/map.tsx
+++ src/shared/ui/map/map.tsx
@@ -1,11 +1,17 @@
 import { Map, YMaps } from '@pbe/react-yandex-maps';
+import { classNames } from '@/shared/lib';
+import styles from './map.module.scss';
 
-const MapView = () => {
+interface MapViewProps {
+  className?: string;
+}
+const MapView = (props: MapViewProps) => {
+  const { className } = props;
+
   return (
     <YMaps>
       <Map
-        width={'100vw'}
-        height={'100vh'}
+        className={classNames(styles.Map, {}, [className])}
         defaultState={{ center: [55.75, 37.57], zoom: 9 }}
       />
     </YMaps>
