6288611b858d8d7e0745f8837311fda4704fb21f
Ilya Perelygin <ilyaperelygin@MacBook-Pro.local>
2023-10-14 17:16:08 +0300

    fix sidebar

--- src/widgets/sideBarOffice/index.tsx
+++ src/widgets/sideBarOffice/index.tsx
@@ -1,12 +1,16 @@
 import { observer } from 'mobx-react-lite';
 import styles from './sidearOffice.module.scss';
 import { useLocalOfficeInfoStore } from '@/entities/officeInfo/model/store';
+import { OfficeList } from '@/entities';
 
 const SideBarOffice = observer(() => {
   const { officeInfoStore } = useLocalOfficeInfoStore();
   return (
-    <div>
+    <div className={styles.root}>
       <div className={styles.headerText}>{officeInfoStore.office?.address}</div>
+      <div>
+        <OfficeList />
+      </div>
     </div>
   );
 });
