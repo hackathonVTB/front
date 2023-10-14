d9f032885e7143c19958e060d5c5b0c8c504d02f
Maksim Doronkin <maksim3602@gmail.com>
2023-10-14 03:23:41 +0300

    add sidebar

--- src/pages/map/map.tsx
+++ src/pages/map/map.tsx
@@ -3,6 +3,7 @@ import { MainLayout } from '@/shared/ui/layouts/main-layout/main-layout.tsx';
 import { Navbar } from '@/widgets';
 import { Drawer } from '@/shared';
 import { isMobile } from 'react-device-detect';
+import { Sidebar } from '@/widgets/sidebar';
 
 const Map = () => {
   return (
@@ -10,6 +11,7 @@ const Map = () => {
       navbar={<Navbar />}
       content={<MapView />}
       drower={isMobile ? <Drawer isOpen={true}>drawer</Drawer> : undefined}
+      leftSidebar={!isMobile ? <Sidebar>Sidebar</Sidebar> : undefined}
     />
   );
 };
